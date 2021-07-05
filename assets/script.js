dbaadb77df23aa7d62a4a349ceeb86bff550f50e
KevinPMyers <kmyers25@gmail.com>
2021-07-04 22:48:34 -0400

    Partial functionality, saves and displays cities, cant get UV to work and local storage wont actually clear

--- assets/script.js
+++ assets/script.js
@@ -4,6 +4,11 @@ var nameEl = document.getElementById("city-display");
 var api = "https://api.openweathermap.org/data/2.5/weather?q=";
 var apiKey = "&appid=9796890990a8a96f5f4b6c8840d069b8";
 var units = "&units=imperial";
+var cityIcon = document.getElementById("city-pic");
+var temperature = document.getElementById("temp-display");
+var wind = document.getElementById("wind-display");
+var humidity = document.getElementById("humid-display");
+var UV = document.getElementById("uv-display");
 var clearSearch = document.getElementById("clear-history");
 var historyEl = document.getElementById("search-history");
 var searchHistory = JSON.parse(localStorage.getItem("search")) || [];
@@ -17,8 +22,36 @@ function getWeather(inputEl) {
    axios.get(url)
    .then(function(response){
        console.log(response);
-   }) 
-}
+// parse to display forecast
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date
+       var todayDate = new Date(response.data.dt*1000);
+       console.log(todayDate);
+       var day = todayDate.getDate();
+       var month = todayDate.getMonth() + 1;
+       var year = todayDate.getFullYear();
+       nameEl.innerHTML = response.data.name + " (" + month + "/" + day + "/" + year + ") ";
+    //    Fetch and display current weather data
+       var weatherPic = response.data.weather[0].icon
+       cityIcon.setAttribute("src", "https://openweathermap.org/img/w/" + weatherPic + ".png");
+       temperature.innerHTML = "Temperature: " + (response.data.main.temp) + "&#176F";
+       wind.innerHTML = "Wind: " + (response.data.wind.speed) + "MPH";
+       humidity.innerHTML = "Humidity: " + response.data.main.humidity + "%";
+       var lat = response.data.coord.lat;
+       var long = response.data.coord.lat;
+    var uvUrl = "https://api.openweathermap.org/data/2.5/uvi/forecast?lat=" + lat + "&lon=" + long + "appid=" + apiKey;
+        axios.get(uvUrl).then(function(response){
+            var uvIndex = document.createElement("span");
+            uvIndex.setAttribute("class", "badge badge-danger");
+            uvIndex.innerHTML = response.data[0].value;
+            UV.innerHTML = "UV Index: ";
+            UV.append(uvIndex);
+        });
+        
+    })
+
+
+
+   } 
 
 // function getData(data) {
 //     nameEl = data
@@ -36,7 +69,8 @@ searchEl.addEventListener("click", function() {
 
 clearSearch.addEventListener("click", function() {
     searchHistory = [];
-    renderSearchHistory();
+    renderSearchHistory(localStorage.clear);
+    ;
 })
 
 function renderSearchHistory() {
@@ -51,7 +85,7 @@ for (let i=0; i<searchHistory.length; i++) {
     historyVal.addEventListener("click", function() {
         getWeather(historyVal.value);
     })
-    historyVal.append(historyEl);
+    historyEl.append(historyVal);
 }
 }
 
