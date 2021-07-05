c13a81d19cae176ce7e8caf4d233e1e9d7ef009b
KevinPMyers <kmyers25@gmail.com>
2021-07-04 20:27:37 -0400

    saving to test why function is not getting a value for search history, trouble shooting now

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,7 @@ var searchEl = document.getElementById("search-btn");
 var nameEl = document.getElementById("city-display");
 var api = "https://api.openweathermap.org/data/2.5/weather?q=";
 var apiKey = "&appid=9796890990a8a96f5f4b6c8840d069b8";
-var units = "&units=standard";
+var units = "&units=imperial";
 var clearSearch = document.getElementById("clear-history");
 var historyEl = document.getElementById("search-history");
 var searchHistory = JSON.parse(localStorage.getItem("search")) || [];
@@ -43,6 +43,19 @@ function renderSearchHistory() {
 historyEl.innerHTML = "";
 for (let i=0; i<searchHistory.length; i++) {
     var historyVal = document.createElement("input");
-    
+
+    historyVal.setAttribute("type", "text");
+    historyVal.setAttribute("readonly", true);
+    historyVal.setAttribute("class", "form-control d-block bg-white");
+    historyVal.setAttribute("value", searchHistory[i]);
+    historyVal.addEventListener("click", function() {
+        getWeather(historyVal.value);
+    })
+    historyVal.append(historyEl);
 }
+}
+
+renderSearchHistory();
+if (searchHistory.length > 0) {
+    getWeather(searchHistory[searchHistory.length - 1]);
 }
